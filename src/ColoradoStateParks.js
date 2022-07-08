f86270973289967da83770391f5404dfa25ba682
k-koech <kelvin.koech@student.moringaschool.com>
2022-07-08 14:59:30 +0300

    Done.

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,9 +1,11 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import { trees, wildlife } from "./parks/RockyMountain";
 
 export default function ColoradoStateParks() 
 {
   howManyParks(); // => "42 parks!"
-
+  // trees();
+  wildlife();
   return <h1>Colorado State Parks!</h1>;
 }
